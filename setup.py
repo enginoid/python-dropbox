0cf910951bd8ec0d53041f98d3b414387ad0d0f8
Friðrik Már Jónsson <fridrik@pyth.net>
2012-04-08 22:03:06 +0000

    Initial commit of official code (v1.4 - March 26, 2012)

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import sys
+
+INSTALL_REQUIRES = ['oauth', 'simplejson']
+
+if sys.version_info < (2,6):
+    INSTALL_REQUIRES.append('ssl')  # This module is built in to Python 2.6+
+
+#must be called dropbox-client so it overwrites the older dropbox SDK
+setup(name='dropbox',
+      version='1.4',
+      description='Official Dropbox REST API Client',
+      author='Dropbox, Inc.',
+      author_email='support-api@dropbox.com',
+      url='http://www.dropbox.com/',
+      packages=['dropbox'],
+      install_requires=INSTALL_REQUIRES,
+      package_data={'dropbox': ['trusted-certs.crt']}
+     )
